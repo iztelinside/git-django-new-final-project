9de2ddfcd6abe8a9140f968e0866bb14fe751c64
Daniyar Izteleuov <d.iztel@gmail.com>
2025-11-15 18:48:31 +0500

    second commit

--- it/main/views.py
+++ it/main/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 def index(request):
-    return HttpResponse("<h3>We are the champions</h3>")
+    return render(request, 'main/index.html')
+# def index(request):
+#     return HttpResponse("<h3>We are the champions</h3>")
+
+
 def about(request):
     return HttpResponse("<h3>We are the super Codemakers!</h3>")
 # Create your views here.
