841838ea593f79bf02c778cbb896715286873549
Daniyar Izteleuov <d.iztel@gmail.com>
2025-11-15 15:07:05 +0500

    first change

---
+++ it/main/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def index(request):
+    return HttpResponse("<h3>We are the champions</h3>")
+def about(request):
+    return HttpResponse("<h3>We are the super Codemakers!</h3>")
+# Create your views here.
+
+
