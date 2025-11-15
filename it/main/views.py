48fd9d9c310acca468da9db276c59dfaf247f606
Daniyar Izteleuov <d.iztel@gmail.com>
2025-11-15 20:01:16 +0500

    Jinja template engine and HTML templates

--- it/main/views.py
+++ it/main/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 def index(request):
     return render(request, 'main/index.html')
 # def index(request):
 #     return HttpResponse("<h3>We are the champions</h3>")
 
-
 def about(request):
-    return HttpResponse("<h3>We are the super Codemakers!</h3>")
+    return render(request, 'main/about.html')
+# def about(request):
+#     return HttpResponse("<h3>We are the super Codemakers!</h3>")
 # Create your views here.
 
 
